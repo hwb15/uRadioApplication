498379068afb08de2d6096be199dbcbba21e86f5
hwb15 <47414652+hwb15@users.noreply.github.com>
2020-03-03 17:01:14 +0000

    Added the following - 03/03/2020:
    ☑️ Added in the toolbar settings icon
    ☑️ Adapted arrayAdapter to colour each ListView row alternately

--- app/src/main/java/com/example/uradio/MainActivity.java
+++ app/src/main/java/com/example/uradio/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.uradio;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
@@ -18,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     // Variable declarations
     ListView stationList;
+    TextView title_bar;
     View bottom_player;
 
+    @SuppressWarnings("unchecked")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,23 +35,34 @@ public class MainActivity extends AppCompatActivity {
         actionBar.hide();
 
         // Initalising the station listview
-
         stationList=(ListView)findViewById(R.id.stationList);
-
         final ArrayList<String> stations = new ArrayList<>();
 
+        // Adding items into the stationList array
         stations.add("Station 1");
         stations.add("Station 2");
         stations.add("Station 3");
-        stations.add("Station 4");
-        stations.add("Station 5");
-        stations.add("Station 6");
-        stations.add("Station 7");
-        stations.add("Station 8");
-        stations.add("Station 9");
         stations.add("+ Add a station here");
 
-        ArrayAdapter stationAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stations);
+
+        ArrayAdapter stationAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stations) {
+            // Alternating the colour in the List View
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                // Get current item from the stationList
+                View view = super.getView(position, convertView, parent);
+                if (position % 2 == 1) {
+                    // Set background color for first row
+                    view.setBackgroundColor(Color.parseColor("#66D5D5D5"));
+                } else {
+                    // Set background color for alternate row
+                    view.setBackgroundColor(Color.parseColor("#66000000"));
+                }
+
+                return view;
+            }
+        };
+
         stationList.setAdapter(stationAdapter);
 
         stationList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
