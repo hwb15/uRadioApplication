189debfe9cf8ff66cf9ff42c7ad22508ab14a5c7
hwb15 <47414652+hwb15@users.noreply.github.com>
2020-03-24 17:11:11 +0000

    Added the following - 23/03/2020:
    ☑️ Changes to validation of station URL and station name in add station activity

--- app/src/main/java/com/example/uradio/addStationActivity.java
+++ app/src/main/java/com/example/uradio/addStationActivity.java
@@ -7,16 +7,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class addStationActivity extends AppCompatActivity {
 
     String stationurl;
     String stationname;
-    boolean cansave;
+
+    private EditText user_stationname_input, user_station_input;
+    private Button saveStation_btn, cancelInput_btn;
+    private TextInputLayout inputLayoutStationName, inputLayoutStationURL;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,35 +38,23 @@ public class addStationActivity extends AppCompatActivity {
         // initializing variable;
         stationurl = "default";
         stationname = "default";
-        cansave = false;
 
         // Save button actions
 
         // initializing the view using findViewById
 
-        final Button saveStation_btn = (Button) findViewById(R.id.savestation_button);
-        Button cancelInput_btn = (Button) findViewById(R.id.cancelstation_button);
 
-        final EditText user_station_input = (EditText) findViewById(R.id.radiostation_userinput);
-        final EditText user_stationname_input = (EditText) findViewById(R.id.radiostationname_userinput);
+        // text input layouts
+        inputLayoutStationName = (TextInputLayout) findViewById(R.id.inputLayoutStationName);
+        inputLayoutStationURL = (TextInputLayout) findViewById(R.id.inputLayoutStationURL);
+        //buttons
+        saveStation_btn = (Button) findViewById(R.id.savestation_button);
+        cancelInput_btn = (Button) findViewById(R.id.cancelstation_button);
+        // edit text fields
+        user_station_input = (EditText) findViewById(R.id.radiostation_userinput);
+        user_stationname_input = (EditText) findViewById(R.id.radiostationname_userinput);
 
 
-        // Validation rules for the text fields
-
-        if (user_stationname_input.getText().toString().length() == 0 || user_stationname_input.getText().toString().length() > 20) {
-            user_stationname_input.setError("The station name has to be within the range of 1 - 20 characters! ");
-        } else {
-            user_stationname_input.setError(null);
-        }
-
-        String station_url_inputted = user_station_input.getText().toString();
-
-        if (user_station_input.getText().toString().isEmpty() || !user_station_input.getText().toString().startsWith("http://") || !user_station_input.getText().toString().startsWith("https://")) {
-            user_station_input.setError("Enter a valid url, starting with 'http://' or 'https://'! ");
-        } else {
-            user_station_input.setError(null);
-
-        }
 
         // Save down the station and return to the main activity -
         // passing the station url that the user inputted
@@ -68,14 +63,8 @@ public class addStationActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                            // Saving the http to string to be passed to main activity
-                            MainActivity.new_station = true;
-                            stationname = user_stationname_input.getText().toString();
-                            stationurl = user_station_input.getText().toString();
-                            Intent passStation = new Intent(addStationActivity.this, MainActivity.class);
-                            passStation.putExtra("stationurl", stationurl);
-                            passStation.putExtra("stationname", stationname);
-                            startActivity(passStation);
+                        validateStation();
+
                     }
                 }
         );
@@ -85,13 +74,48 @@ public class addStationActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        // Changing back to the main activity
-                        MainActivity.new_station = false;
-                        Intent cancelStation = new Intent(addStationActivity.this, MainActivity.class);
-                        startActivity(cancelStation);
+                        cancelSaveStation();
                     }
                 }
         );
     }
 
+    private void validateStation() {
+        boolean isValid = true;
+        if (user_stationname_input.getText().toString().isEmpty()) {
+            inputLayoutStationName.setError("The station name is empty! Fill this in!");
+            isValid = false;
+        } else {
+            inputLayoutStationName.setErrorEnabled(false);
+        }
+
+        if (user_station_input.getText().toString().startsWith("http://") || user_station_input.getText().toString().startsWith("https://")) {
+            inputLayoutStationURL.setErrorEnabled(false);
+        } else {
+            inputLayoutStationURL.setError("Station URL needs to start with either 'http://' or 'https://'");
+            isValid = false;
+        }
+
+        if (isValid) {
+            saveStation();
+        }
+    }
+
+    private void saveStation() {
+        // Saving the http to string to be passed to main activity
+        MainActivity.new_station = true;
+        stationname = user_stationname_input.getText().toString();
+        stationurl = user_station_input.getText().toString();
+        Intent passStation = new Intent(addStationActivity.this, MainActivity.class);
+        passStation.putExtra("stationurl", stationurl);
+        passStation.putExtra("stationname", stationname);
+        startActivity(passStation);
+    }
+
+    private void cancelSaveStation() {
+        // Changing back to the main activity
+        MainActivity.new_station = false;
+        Intent cancelStation = new Intent(addStationActivity.this, MainActivity.class);
+        startActivity(cancelStation);
+    }
 }
